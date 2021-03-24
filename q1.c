2d63801842de22bab5ecf1c931b7cbd28cb1a763
Umang Srivastava <umang.srivastava@students.iiit.ac.in>
2021-03-24 07:24:26 +0530

    Fixed value range

--- q1.c
+++ q1.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include <time.h>
+// #include <time.h>
 
 #define N 1001
 #define BS 16
@@ -39,7 +39,7 @@ int take_input(Matrix * restrict arr[])
 		{
 			for(k=0;k<y;++k)
 			{
-				fscanf(fptr,"%d",&mat->matrix[j][k]);
+				fscanf(fptr,"%ld",&mat->matrix[j][k]);
 			}
 		}
 		arr[i] = mat;
@@ -64,7 +64,7 @@ void print_matrix(Matrix * restrict result)
 	{
 		for(j=0;j<result->m;++j)
 		{
-			fprintf(fptr,"%d ", result->matrix[i][j]);
+			fprintf(fptr,"%ld ", result->matrix[i][j]);
 		}
 		fprintf(fptr,"\n");
 	}
